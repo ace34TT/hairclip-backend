58b559a068002eb5846f1c5fb31ae3ea5b9f97d0
ace34TT <tafinasoa35@gmail.com>
2023-03-29 20:30:59 +0300

    cors

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -18,20 +18,20 @@ class OrderController extends Controller
     public function generateClient(Request $request)
     {
         $total = $request->get("total");
-        // try {
-        Stripe::setApiKey(env(key: "SK_STRIPE"));
-        $paymentIntent = PaymentIntent::create([
-            "amount" =>  $total * 100,
-            'currency' => 'eur',
-        ]);
-        $output = [
-            'clientSecret' => $paymentIntent->client_secret,
-        ];
-        return response()->json($output, 200);
-        // } catch (Error $e) {
-        //     return response()->json(["error" => $e->getMessage()], 500);
-        // }
-        // return response()->json(["success" => true]);
+        try {
+            Stripe::setApiKey(env(key: "SK_STRIPE"));
+            $paymentIntent = PaymentIntent::create([
+                "amount" =>  $total * 100,
+                'currency' => 'eur',
+            ]);
+            $output = [
+                'clientSecret' => $paymentIntent->client_secret,
+            ];
+            return response()->json($output, 200);
+        } catch (Error $e) {
+            return response()->json(["error" => $e->getMessage()], 500);
+        }
+        return response()->json(["success" => true]);
     }
 
     public function paymentSuccess(Request $request)
