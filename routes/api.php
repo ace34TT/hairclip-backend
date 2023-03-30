e986341320b05233e5835fe24064e3ed532cac40
ace34TT <tafinasoa35@gmail.com>
2023-03-30 09:15:05 +0300

    cors

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-// header('Access-Control-Allow-Origin: *');
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::get("products/shop", [ProductController::class, "getAllWithPng"]);
 Route::resource('products', ProductController::class);
 Route::get("media/images/{filename}", [MediaController::class, "serveFile"]);
